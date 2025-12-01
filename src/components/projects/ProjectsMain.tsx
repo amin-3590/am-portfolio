d8d8a3550cba5b6e5c4ea64e53d0bfde8fe136e5
amin aliakbari <lyakbryamyn7@gmail.com>
2025-12-02 00:00:15 +0330

    fix projetc 2

--- src/components/projects/ProjectsMain.tsx
+++ src/components/projects/ProjectsMain.tsx
@@ -3,9 +3,14 @@ import ProjectsText from "./ProjectsText";
 
 const ProjectsMain = () => {
   return (
-    <div id="projects" className="max-w-[1150px] mx-auto px-5 py-8 overflow-hidden">
+    <div id="projects" className="max-w-[1150px] mx-auto px-5 py-6 overflow-hidden">
       <ProjectsText />
       <ProjectsImage />
+      <div className="my-20 text-[19px] font-bold text-center md:text-2xl text-gray-300/90">
+        <a href="https://github.com/amin-3590" className="transition-colors border-b-2 hover:text-cyan-300">
+          All Project
+        </a>
+      </div>
     </div>
   );
 };
